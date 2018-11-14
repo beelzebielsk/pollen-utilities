c3b9a4ce1a0539bb47a262a43bf10574fb4fe9ae
adam <ibrahimadam193@gmail.com>
2018-11-13 22:42:44 -0500

    Start adding tests to utility.rkt
    Changes to be committed:
          modified:   utility.rkt

--- pollen-utilities/utility.rkt
+++ pollen-utilities/utility.rkt
@@ -83,10 +83,21 @@
 
 ; list? list? -> lambda
 ; Takes in one of two optional lists of tags: 
-; #:only, which specifies which tags to flatten and nothing else.
-; #:exclude, which specifies flattens all tags but those listed.
+; #:only, which specifies which tags to flatten and nothing else (a
+; whitelist for flattening).
+; #:exclude, which will flattens all tags except for those listed (a
+; blacklist for flattening).
 ; Returns a function that, when given as #:txexpr-proc in
 ; pollen/decode, will flatten those tags and leave all others alone.
+;
+; #:only and #:exclude should not be given together, as the effect of
+; only one of the two will work.
+;
+; Here, flattening means that the contents of the tag will be placed
+; in the parent tag. The flattening here is recursive, so for a given
+; tag which is to be flattened, first all of it's descendants will
+; have their contents flattened (if specified to be), then those
+; results will be placed in the parent tag of that tag.
 (define (decode-flattener #:only [only-these (void)]
                           #:exclude [except-these (void)])
   (cond [(and (void? only-these) (void? except-these))
@@ -213,6 +224,185 @@
   (dropf-right (dropf lst pred?) pred?))
 
 (provide (all-defined-out))
+
+(module+ test
+  (require rackunit)
+  (define (zip l1 l2)
+    (cond ((or (null? l1) (null? l2)) null)
+          (else (cons (list (car l1) (car l2))
+                      (zip (cdr l1) (cdr l2))))))
+  (test-case 
+    "reverse*"
+    (define tests
+      `((a b c)
+        (a b c d)
+        (0 1 2 3 4)
+        (a b 0 1)
+        (a (b c) d e (f g))
+        (a (b c d) 0 1 2 (3 4 5 e f (g h)))))
+    (define expected-results
+      `((c b a)
+        (d c b a)
+        (4 3 2 1 0)
+        (1 0 b a)
+        ((g f) e d (c b) a)
+        (((h g) f e 5 4 3) 2 1 0 (d c b) a)))
+    (for ([test-case tests]
+          [result expected-results])
+      (check-equal? (reverse* test-case) result "reverse* failed")))
+  (test-case
+    "decode-flattener"
+    (define xexp1
+        `(root 
+           (a "content") 
+           (b "content") 
+           (c "content")
+           (d "content")))
+    (match-let ([(list 'root a b c d) xexp1])
+      (check-equal? 
+        `(root ,@(get-elements a) ,b ,c ,d)
+        (decode xexp1 #:txexpr-proc (decode-flattener #:only '(a))))
+      (check-equal? 
+        `(root ,a ,@(get-elements b) ,c ,d)
+        (decode xexp1 #:txexpr-proc (decode-flattener #:only '(b))))
+      (check-equal? 
+        `(root ,a ,b ,@(get-elements c) ,d)
+        (decode xexp1 #:txexpr-proc (decode-flattener #:only '(c))))
+      (check-equal? 
+        `(root ,a ,b ,c ,@(get-elements d))
+        (decode xexp1 #:txexpr-proc (decode-flattener #:only '(d))))
+      (check-equal? 
+        `(root ,@(get-elements a) ,@(get-elements b) ,c ,d)
+        (decode xexp1 #:txexpr-proc (decode-flattener #:only '(a b))))
+      (check-equal? 
+        `(root ,a ,@(get-elements b) ,@(get-elements c) ,d)
+        (decode xexp1 #:txexpr-proc (decode-flattener #:only '(b c))))
+      (check-equal? 
+        `(root ,a ,b ,@(get-elements c) ,@(get-elements d))
+        (decode xexp1 #:txexpr-proc (decode-flattener #:only '(c d))))
+      (check-equal? 
+        `(root ,@(get-elements a) ,b ,c ,@(get-elements d))
+        (decode xexp1 #:txexpr-proc (decode-flattener #:only '(a d))))))
+  (test-case
+    "split-where"
+    (define case1 '(0 1 2 s 3 4 5 s s 6 7 8 9 10 s))
+    (check-equal?
+      (call-with-values
+        (for/fold 
+          ([splits null] [current-split null])
+          ([e case1])
+          (cond 
+            ((eq? e 's) 
+             (if (null? current-split)
+               (values splits null)
+               (values (append splits (list current-split)) null)))
+            (else 
+              (values splits (append current-split (list e))))))
+        (λ (x . _) x))
+      (split-where case1 (λ (e . _) (eq? 's e)))))
+
+  )
+
+
+; list? procedure? 
+;   #:keep-where procedure? 
+;   #:split-map procedure?
+;   #:keep-empty-splits? boolean?
+;   #:action (or/c procedure? #f)
+; -> list?
+; Somewhat similar to the split procedure for strings. Takes a list
+; and returns a list of the same elements of lst, in the same order,
+; but placed in sublists. Each sublist ends where an element occurs
+; that causes (split-pred? element current-split tail) to be true. The
+; next sublist picks up from there. 
+; - The #:keep-where procedure determines where an element that causes
+;   a split will go. If the procedure returns:
+;       - 'current: The element which caused the split will be placed
+;         in the sublist that was being built when the element was
+;         encountered.
+;       - 'next: The element which caused the split will be placed
+;         at the start of the next sublist that will be built up.
+;       - 'separate: The element will be placed on its own. It won't
+;         be in a sublist, the element itself will be placed between
+;         the current sublist being built, and the next sublist that
+;         will be built.
+;       - 'ignore: The element will not be placed in any split, nor on
+;         its own.
+; - The split-map option is supplied because the output of split-where
+;   may not be a list of splits if the #:keep-where function returns
+;   'separate. In this case, the split element is placed on it's own
+;   in the list of splits. Mapping over the splits (and only the
+;   splits) is a common enough use-case, I think, that the optional
+;   parameter is
+; warranted.
+; - If a split should be empty (such as when there are two consecutive
+;   elements that cause split-pred? to be true), then those splits are
+;   not kept if #:keep-empty-splits? is false. Otherwise, the splits
+;   are kept.
+; - #:action is a procedure that should take the following paramters:
+;   current-split, splits, remaining and return (values
+;   next-current-split, next-splits, next-remaining). This allows
+;   total control over how to produce splits from the function, if
+;   that's desired.
+;
+; The reason why the objects at which we split are not placed in the
+; list as splits is that this function takes after split-string, and
+; functions like it from other languages. The thing upon which we
+; split is normally removed. Not considered. There are use cases where
+; you wouldn't want to throw away that which you split upon, but you'd
+; want to run a function over everything else.
+(define (alternate-split-where 
+          lst split-pred?
+          #:keep-where [keep-pred? (λ _ 'ignore)]
+          #:split-map [split-func reverse]
+          #:keep-empty-splits? [keep-empty-splits? #f]
+          #:action [loop-body #f])
+  (define-values (last-split splits _)
+    (if loop-body
+      (for/fold
+        ([current-split null] [splits null] [remaining lst])
+        ([element lst])
+        (loop-body current-split splits remaining))
+      (for/fold 
+        ([current-split null] [splits null] [remaining lst])
+        ([element lst])
+        (define tail (cdr remaining))
+        (define split? (split-pred? element current-split tail))
+        (define decision (keep-pred? element current-split tail))
+        (if (not split?)
+          (values (cons element current-split)
+                  splits
+                  tail)
+          (begin
+            (define next-split
+              (if (eq? decision 'next)
+                (list element)
+                null))
+            (define processed-current-split
+              (if (eq? decision 'current)
+                (split-func (cons element current-split))
+                (split-func current-split)))
+            (define new-splits
+              (match (list decision (null? processed-current-split))
+                [(list 'separate #t)
+                 (cons elem splits)]
+                [(list 'separate #f)
+                 (append (list elem processed-current-split)
+                         splits)]
+                [(list (not 'separate) #t) splits]
+                [(list (not 'separate) #f) 
+                 (cons processed-current-split splits)]))
+            (values next-split new-splits tail)))
+        (values (cons element current-split)
+                splits
+                tail))))
+  (reverse
+    (cond ((null? last-split) splits)
+          (else (cons (split-func last-split) splits)))))
+
+
+
+
 
 ; TODO:
 ; - make let-splice convert numbers to strings in the final product.
